f809e511e0ac50c4483399688536d83c96026c8f
elvis <elvisduru76@gmail.com>
2022-06-16 03:32:36 +0100

    stopwatch

---
+++ timer assignment/time.js
@@ -0,0 +1,43 @@
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function showTime(time,endtime) {
+  const clock = document.querySelector('#time');
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateTime() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateTime();
+  const timeinterval = setInterval(updateTime, 1000);
+}
+
+// const deadline = new Date(Date.parse(new Date()) + 60 * 24 * 60 * 60 * 1000);
+const deadline = '2021-12-25';
+showTime('time', deadline);
